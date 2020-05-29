6c2c38a736c94441e1b28d28abc6b68829e3311b
bruce <brucec@hotmail.com>
2020-05-29 11:38:46 -0700

    add sql setup script

---
+++ src/main/sql/scripts/databaseSetup.sql
@@ -0,0 +1,9 @@
+CREATE TABLE USER (
+    userID INTEGER PRIMARY KEY,
+    fname varchar(20),
+    lname varchar(20),
+    birthdate varchar(20),
+    phone varchar(20),
+    email varchar(20),
+    cityID integer
+);
